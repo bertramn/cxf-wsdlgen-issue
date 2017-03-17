7e005946b0e1e87822022f416740517d2d940c57
Niels Bertram <niels.bertram@virginaustralia.com>
2017-03-18 03:16:19 +1000

    ogiginal example where includes are not working properly when using a jaxb binding file to exclude a whole package

---
+++ src/main/java/some/model/package-info.java
@@ -0,0 +1,4 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "urn:some.model", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED, xmlns = {
+  @javax.xml.bind.annotation.XmlNs(namespaceURI = "urn:some.model", prefix = "")
+})
+package some.model;
